772cc38d1c7d22c6e0e712a7e72ac593b949f851
FernandoPerea <ferperea0@gmail.com>
2021-01-13 20:46:49 -0600

    Nuevo Grupo Listo(Create)

--- php/CreateGrupo.php
+++ php/CreateGrupo.php
@@ -1,76 +1,21 @@
 <?php
     include("conexion.php");
-    $carrera = "0";
-    $academia = "0";
+  
+    $nombre = $_POST['nombre'];
+    $descripcion = $_POST['descripcion'];
 
-    $boleta = $_POST['boleta'];
-    $nombres = $_POST['nombre'];
-    $pa = $_POST['p_apellido'];
-    $sa = $_POST['s_apellido'];
-    $fecha = $_POST['fecha'];
-    $sexo = $_POST['sexo'];
-    $masa = $_POST['peso'];
-    $altura = $_POST['altura'];
-    $sangre = $_POST['sangre'];
-    $seguro = $_POST['hospital'];
-    $nss = $_POST['nss'];
-    $curp = $_POST['CURP'];
-    $tel = $_POST['TEL'];
-    $tel_e = $_POST['TEL_EMERGENCIA'];
-    $contacto_e = $_POST['NOM_TEL_EMERGENCIA'];
-    $tipo = $_POST['tipo_paciente'];
-    if (isset($_POST['CARRERA'])) {
-        $carrera = $_POST['CARRERA'];
-    }
-    if (isset($_POST['Academia'])) {
-        $academia = $_POST['Academia'];
-    }
-    $ah = $_POST['ah'];
-    $apnp = $_POST['apnp'];
-    $app = $_POST['app'];
-    $od = $_POST['od'];
-
-    if ($tipo == "3") {
-        $carrera = "0";
-        $academia = "0";
-    } elseif ($tipo == "2") {
-        $carrera = "0";
-    } elseif ($tipo == "1") {
-        $academia = "0";
-    }
-
-    $consulta  = "INSERT INTO `paciente` (`id_paciente`, `boleta`, `nombres`, `pa`, `sa`, `fecha`, `sexo`, `masa`, `altura`, `sangre`, `seguro`, `nss`, `curp`, `tel`, `tel_e`, `contacto_e`, `tipo`, `carrera`, `academia`, `ah`, `apnp`, `app`, `od`)";
+    $consulta  = "INSERT INTO `grupo` (`idGrupo`, `nombre`, `descripcion`)";
     $consulta  .= "VALUES (NULL, ";
-    $consulta  .= "'".$boleta."',";
-    $consulta  .= "'".$nombres."',";
-    $consulta  .= "'". $pa ."',";
-    $consulta  .= "'". $sa ."',";
-    $consulta  .= "'". $fecha ."',";
-    $consulta  .= "'". $sexo ."',";
-    $consulta  .= "'". $masa ."',";
-    $consulta  .= "'". $altura ."',";
-    $consulta  .= "'". $sangre ."',";
-    $consulta  .= "'". $seguro ."',";
-    $consulta  .= "'". $nss ."',";
-    $consulta  .= "'". $curp ."',";
-    $consulta  .= "'". $tel ."',";
-    $consulta  .= "'". $tel_e ."',";
-    $consulta  .= "'". $contacto_e ."',";
-    $consulta  .= "'". $tipo ."',";
-    $consulta  .= "'". $carrera."',";
-    $consulta  .= "'". $academia."',";
-    $consulta  .= "'". $ah."',";
-    $consulta  .= "'". $apnp."',";
-    $consulta  .= "'". $app."',";
-    $consulta  .= "'". $od."')";
+    $consulta  .= "'".$nombre."',";
+    $consulta  .= "'". $descripcion."')";
 
     $respuesta = array();
     $link=connect();
     if (mysqli_query($link, $consulta)) {
-        $respuesta['resultado'] = "si";
+        $status = "ok";
     } else {
-        $respuesta['resultado'] = "no";
+        $status = "err";
     }
     mysqli_close($link);
-
+    echo $status;die;
     echo json_encode($respuesta);
