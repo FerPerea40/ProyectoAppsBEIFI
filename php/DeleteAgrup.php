0eff11603018b46c70d961355e2d3fa32acae3e8
Daniel-Torres05 <dan00005.zac@gmail.com>
2021-01-15 01:25:21 -0600

    as

---
+++ php/DeleteAgrup.php
@@ -0,0 +1,20 @@
+<?php
+   include("conexion.php");
+
+    $id=$_POST['id'];
+    
+
+     if(isset($id)){
+    $consulta="delete from agrupamiento where idAgrupamiento=".$id;
+	
+	$link=connect();
+       
+    if (mysqli_query($link, $consulta)) {
+        $status = "ok";
+    } else {
+        $status = "err";
+    }
+    mysqli_close($link);
+    echo $status;die;}
+
+	?>
