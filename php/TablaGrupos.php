be59bae11dba92c3c349cc7acd7ead040452fd80
FernandoPerea <ferperea0@gmail.com>
2021-01-15 15:24:04 -0600

    wer

--- php/TablaGrupos.php
+++ php/TablaGrupos.php
@@ -4,12 +4,30 @@
 	$consulta="SELECT * from grupo";
 	$link=connect();
     $respuesta = mysqli_query($link, $consulta) or die("Error al ejecutar la consulta");
-    mysqli_close($link);
-
-    $rows = array();
+    
+    $tabla = "";
+    
     while ($r = mysqli_fetch_assoc($respuesta)) {
+        /*$ver =  " <div class='form-group text-center'>
+        <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#modal-editar' value='".$r['idGrupo']."' onclick='selUpdateGrupo(".$r['idGrupo'].")'' '>
+         Editar</button>";
+        $ver2="<button id='btn' type='button' class='btn btn-danger' data-toggle='modal' data-target='#modal-eliminar'  value='".$r['idGrupo']."' onclick='fnselect(".$r['idGrupo'].")'' '>
+        Eliminar</button>";
+        $ver3="<button type='button' class='btn btn-warning' data-toggle='modal' data-target='#modal-asignar' value='".$r['idGrupo']."' onclick='selAsigGrupo(".$r['idGrupo'].")'' '>
+         Asignar</button></div>" ;*/
+        $ver='<a data-toggle=\"tooltip\" data-placement=\"top\" title=\"Editar\" class=\"btn btn-primary\"><i class=\"fa fa-pencil\" aria-hidden=\"true\"></i></a>';
+
+        
         $rows[] = $r;
+        $tabla.='{
+            "Nombre":"'.$r['nombre'].'",
+            "Descripcion":"'.$r['descripcion'].'",
+            "Accion":"'.$ver.'"
+          },';
     }
-    echo json_encode($rows);
+    $tabla = substr($tabla,0, strlen($tabla) - 1);
+
+	echo '{"data":['.$tabla.']}';	
+    mysqli_close($link);
 
 	?>
