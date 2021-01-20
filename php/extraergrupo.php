b807e9efa432a868e74a8b5ed1b58520c81e7cec
CarlitosObr <cdelgado1800@alumno.ipn.mx>
2021-01-19 20:22:59 -0600

    Ya jala el seleccione

--- php/extraergrupo.php
+++ php/extraergrupo.php
@@ -4,7 +4,7 @@
     $consulta = "SELECT * from grupo";
     $query = mysqli_query($con,$consulta);
 
-    echo '<option></option>';
+    echo '<option>Seleccione el grupo</option>';
         while ($fila = mysqli_fetch_array($query,MYSQLI_ASSOC)) {
  
             echo '<option value="'.$fila['idGrupo'].'">'.$fila['nombre'].'</option>';
